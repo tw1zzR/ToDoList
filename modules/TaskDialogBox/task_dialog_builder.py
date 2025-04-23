541348323f1be8fff66e03d8164b862113f4dfd1
tw1zzR <regenuss@gmail.com>
2025-04-23 21:10:21 +0300

    Final optimize in TaskDialogBox. Deleted extra code, improved code readability

--- modules/TaskDialogBox/task_dialog_builder.py
+++ modules/TaskDialogBox/task_dialog_builder.py
@@ -1,13 +1,12 @@
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-
 from modules.TaskDialogBox import dialog_tools
-
+from PyQt5.QtGui import *
 
 def setup_UI(task_dialog):
     setup_task_dialog(task_dialog)
+    setup_input_fields(task_dialog)
     setup_connections(task_dialog)
+    setup_button_layout(task_dialog)
+    setup_main_layout(task_dialog)
     setup_what_this_button(task_dialog)
     setup_object_names(task_dialog)
 
@@ -16,10 +15,54 @@ def setup_task_dialog(task_dialog):
     task_dialog.setWindowIcon(QIcon("assets/TaskDialogBox/addtask_dialogbox_icon.png"))
     task_dialog.resize(520, 600)
 
+def setup_input_fields(task_dialog):
+    task_dialog.user_input_task_name.setMaxLength(24)
+    task_dialog.user_input_task_name.setPlaceholderText("Enter task name")
+
+    task_dialog.user_input_task_deadline.setDateTime(task_dialog.current_time)
+    task_dialog.user_input_task_deadline.setMinimumDateTime(task_dialog.current_time)
+
+    task_dialog.user_input_task_description.setPlaceholderText("Enter task description")
+
+    task_dialog.user_input_task_name.setMinimumHeight(50)
+    task_dialog.user_input_task_deadline.setMinimumHeight(50)
+    task_dialog.user_input_task_description.setMinimumHeight(50)
+
 def setup_connections(task_dialog):
     task_dialog.user_input_task_description.textChanged.connect(lambda: dialog_tools.limit_max_chars_in_textedit(task_dialog))
     task_dialog.send_button.clicked.connect(lambda: dialog_tools.get_task_data(task_dialog))
     task_dialog.cancel_button.clicked.connect(task_dialog.reject)
+
+def setup_button_layout(task_dialog):
+    task_dialog.button_layout.setContentsMargins(0, 0, 0, 0)
+    task_dialog.button_layout.setSpacing(0)
+
+    task_dialog.button_layout.addWidget(task_dialog.send_button)
+    task_dialog.button_layout.addSpacing(5)
+    task_dialog.button_layout.addWidget(task_dialog.cancel_button)
+
+    task_dialog.button_widget.setLayout(task_dialog.button_layout)
+
+def setup_main_layout(task_dialog):
+    task_dialog.main_layout.setContentsMargins(50, 50, 50, 50)
+    task_dialog.main_layout.setSpacing(0)
+
+    task_dialog.main_layout.addWidget(task_dialog.task_name_label)
+    task_dialog.main_layout.addWidget(task_dialog.user_input_task_name)
+    task_dialog.main_layout.addSpacing(25)
+
+    task_dialog.main_layout.addWidget(task_dialog.task_deadline_label)
+    task_dialog.main_layout.addWidget(task_dialog.user_input_task_deadline)
+    task_dialog.main_layout.addSpacing(25)
+
+    task_dialog.main_layout.addWidget(task_dialog.task_description_label)
+    task_dialog.main_layout.addWidget(task_dialog.user_input_task_description)
+    task_dialog.main_layout.addSpacing(25)
+
+    task_dialog.main_layout.addWidget(task_dialog.button_widget)
+
+    task_dialog.setLayout(task_dialog.main_layout)
+
 
 def setup_what_this_button(task_dialog):
     task_dialog.setWhatsThis("It's a dialog box that allows you to enter data for your task.")
--- windows/TaskDialogBox.py
+++ windows/TaskDialogBox.py
@@ -12,8 +12,12 @@ class TaskDialogBox(QDialog):
         self.visual_changer = ChangeVisualUI(self)
 
         self.dark_theme = True
+        self.current_time = QDateTime.currentDateTime()
+        self.max_chars = 150
 
         self.main_layout = QVBoxLayout()
+        self.button_widget = QWidget(self)
+        self.button_layout = QHBoxLayout(self.button_widget)
 
         self.task_name_label = QLabel("Task name", self)
         self.task_deadline_label = QLabel("Deadline", self)
@@ -23,75 +27,12 @@ class TaskDialogBox(QDialog):
         self.user_input_task_deadline = QDateTimeEdit(self)
         self.user_input_task_description = QTextEdit(self)
 
-        self.button_widget = QWidget(self)
-        self.button_layout = QHBoxLayout(self.button_widget)
-
         self.send_button = QPushButton("SEND")
         self.cancel_button = QPushButton("CANCEL")
 
-        self.current_time = QDateTime.currentDateTime()
-        self.max_chars = 150
+        self.init_UI()
 
-        self.initUI()
-
-    def initUI(self):
-        self.user_input_task_name.setMaxLength(24)
-        self.user_input_task_name.setPlaceholderText("Enter task name")
-
-        self.user_input_task_deadline.setDateTime(self.current_time)
-        self.user_input_task_deadline.setMinimumDateTime(self.current_time)
-
-        self.user_input_task_description.setPlaceholderText("Enter task description")
-
-
-        # -----------------------------------------------
-        self.user_input_task_name.setMinimumHeight(50)
-        self.user_input_task_deadline.setMinimumHeight(50)
-        self.user_input_task_description.setMinimumHeight(50)
-
-
-        self.button_layout.setContentsMargins(0, 0, 0, 0)
-        self.button_layout.setSpacing(0)
-
-        self.button_layout.addWidget(self.send_button)
-        self.button_layout.addSpacing(5)
-        self.button_layout.addWidget(self.cancel_button)
-
-        self.button_widget.setLayout(self.button_layout)
-
-        self.main_layout.setContentsMargins(50, 50, 50, 50)
-        self.main_layout.setSpacing(0)
-
-        self.main_layout.addWidget(self.task_name_label)
-        self.main_layout.addWidget(self.user_input_task_name)
-        self.main_layout.addSpacing(25)
-
-        self.main_layout.addWidget(self.task_deadline_label)
-        self.main_layout.addWidget(self.user_input_task_deadline)
-        self.main_layout.addSpacing(25)
-
-        self.main_layout.addWidget(self.task_description_label)
-        self.main_layout.addWidget(self.user_input_task_description)
-        self.main_layout.addSpacing(25)
-
-        self.main_layout.addWidget(self.button_widget)
-
-        self.setLayout(self.main_layout)
-
-        # self.task_name_label.move(50, 50)
-        # self.user_input_task_name.setGeometry(50,75,420,50)
-        #
-        # self.task_deadline_label.move(50, 150)
-        # self.user_input_task_deadline.setGeometry(50,175,420,50)
-        #
-        # self.task_description_label.move(50, 250)
-        # self.user_input_task_description.setGeometry(50,275,420,200)
-        #
-        # self.button_widget.setGeometry(50,500,420,50)
-
-
-
-        # -------------------------------------
+    def init_UI(self):
         task_dialog_builder.setup_UI(self)
         self.visual_changer.change_UI_theme()
         self.show()
